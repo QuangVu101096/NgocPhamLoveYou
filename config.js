a05722169d44887d37af0d2bdfe73ad850842e0d
QuangVu101096 <117662368+QuangVu101096@users.noreply.github.com>
2022-11-08 00:55:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -10,11 +10,11 @@ const CONFIG = {
     desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu bạn <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    messDesc: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/profile.php?id=100023637410534' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
