b8c0a0c416060f42d8060d52ba1a066f9cccc006
QuangVu101096 <117662368+QuangVu101096@users.noreply.github.com>
2022-11-08 23:24:12 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -68,44 +68,63 @@ function moveButton() {
     var top = y + 'px';
     $('#no').css("left", left);
     $('#no').css("top", top);
+	
 }
 
 init()
 
 var n = 0;
-$('#no').mousemove(function() {
-    if (Math.random() < 0.5 || n == 1)
-        switchButton();
-    else
-        moveButton();
-    n++;
-});
-$('#no').click(() => {
-    if (screen.width >= 900)
-        switchButton();
-})
+//$('#no').mousemove(function() {
+//    if (Math.random() < 0.5 || n == 1)
+//        switchButton();
+//    else
+//        moveButton();
+//    n++;
+//});
+//$('#no').click(() => {
+//    if (screen.width >= 900)
+//        switchButton();
+//})
 
 // generate text in input
-function textGenerate() {
-    var n = "";
-    var text = " " + CONFIG.reply;
-    var a = Array.from(text);
-    var textVal = $('#txtReason').val() ? $('#txtReason').val() : "";
-    var count = textVal.length;
-    if (count > 0) {
-        for (let i = 1; i <= count; i++) {
-            n = n + a[i];
-            if (i == text.length + 1) {
-                $('#txtReason').val("");
-                n = "";
-                break;
-            }
-        }
-    }
-    $('#txtReason').val(n);
-    setTimeout("textGenerate()", 1);
-}
-
+//function textGenerate() {
+//    var n = "";
+//   var text = " " + CONFIG.reply;
+//    var a = Array.from(text);
+//    var textVal = $('#txtReason').val() ? $('#txtReason').val() : "";
+//    var count = textVal.length;
+//    if (count > 0) {
+//        for (let i = 1; i <= count; i++) {
+//            n = n + a[i];
+//            if (i == text.length + 1) {
+//                $('#txtReason').val("");
+//               n = "";
+//                break;
+//            }
+//        }
+//    }
+//    $('#txtReason').val(n);
+ //   setTimeout("textGenerate()", 1);
+//}
+$('#no').click(function() {
+	Swal.fire({
+        title: CONFIG.back,
+		icon: 'error',
+        html: true,
+        width: 700,
+        padding: '2em',
+		html: "",
+		background: '#fff url("img/iput-bg.jpg")',		
+        backdrop: `
+              rgba(0,0,123,0.4)
+              url("img/giphy2.gif")
+              left top
+              no-repeat
+            `,
+		showConfirmButton: false,
+		timer: 5000
+    })
+})
 // show popup
 $('#yes').click(function() {
     var audio = new Audio('sound/tick.mp3');
@@ -133,7 +152,7 @@ $('#yes').click(function() {
                 confirmButtonText: CONFIG.btnAccept,
                 background: '#fff url("img/iput-bg.jpg")',
                 title: CONFIG.mess,
-                text: CONFIG.messDesc,
+                text:  CONFIG.messDesc,
                 confirmButtonColor: '#83d0c9',
                 onClose: () => {
                     window.location = CONFIG.messLink;
